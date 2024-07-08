3a3ae6b838de0efbeed9f8ed52507efe2206c6a6
Dmitry Dorogoy <Dmitrii_Dorogoi@epam.com>
2024-07-08 23:29:21 +0200

    String diff preview added

--- Demo/Stringdiffdemo/HighlightedStringReconstructor.cs
+++ Demo/Stringdiffdemo/HighlightedStringReconstructor.cs
@@ -1,17 +1,16 @@
-using System.Text;
 using System.Net;
-
 
 namespace DimonSmart.StringDiff
 {
     public class HighlightedStringReconstructor : StringReconstructor
     {
-        private const string InsertedTextClass = "inserted-text";
-        private const string DeletedTextClass = "deleted-text";
-        private const string ModifiedTextClass = "modified-text";
+        private const string InsertedTextClass = "diffviewbox-inserted-text";
+        private const string DeletedTextClass = "diffviewbox-deleted-text";
+        private const string ModifiedTextClass = "diffviewbox-modified-text";
+        private const string UnchangedTextClass = "diffviewbox-unchanged-text";
 
         protected override string FormatInsertedText(string text) =>
-            $"<span class='{InsertedTextClass}'>{EscapeAndFormatText(text)}</span>";
+            $"<span cla{InsertedTextClass}' >{EscapeAndFormatText(text)}</span>";
 
         protected override string FormatDeletedText(string text) =>
             $"<span class='{DeletedTextClass}'>{EscapeAndFormatText(text)}</span>";
@@ -19,16 +18,12 @@ namespace DimonSmart.StringDiff
         protected override string FormatModifiedText(string oldText, string newText) =>
             $"<span class='{DeletedTextClass}'>{EscapeAndFormatText(oldText)}</span><span class='{ModifiedTextClass}'>{EscapeAndFormatText(newText)}</span>";
 
-        private string EscapeAndFormatText(string text)
+        protected override string FormatUnchangedText(string text) =>
+            $"<span class='{UnchangedTextClass}'>{EscapeAndFormatText(text)}</span>";
+
+        private static string EscapeAndFormatText(string text)
         {
-            if (string.IsNullOrEmpty(text))
-                return text;
-
-            // Use WebUtility.HtmlEncode to escape HTML special characters
-            var escapedText = WebUtility.HtmlEncode(text);
-
-            // Replace newlines with <br> tags
-            return escapedText.Replace("\n", "<br>");
+            return string.IsNullOrEmpty(text) ? text : WebUtility.HtmlEncode(text).Replace("\n", "<br>");
         }
     }
 }
