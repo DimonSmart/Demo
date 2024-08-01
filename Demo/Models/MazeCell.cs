f1466fe44a3115b062e7240f46570dc5282eaa3f
Dmitry Dorogoy <DimonSmart@users.noreply.github.com>
2024-08-02 00:03:37 +0200

    Maze (#1)
    * WIP
    
    * Maze demo WIP
    
    * Maze draw WIP
    
    * MazeGenerator updated
    
    * Maze demo updated
    
    * Updated MazeGenerator nuget
    
    * Maze Generator demo added.

---
+++ Demo/Models/MazeCell.cs
@@ -0,0 +1,13 @@
+﻿using DimonSmart.MazeGenerator;
+
+namespace Demo.Models
+{
+    public class MazeCell : ICell
+    {
+        private bool _wall = false;
+
+        public bool IsWall() => _wall;
+
+        public void MakeWall() => _wall = true;
+    }
+}
