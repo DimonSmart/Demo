3680e3326762fd766b2a1d45e7229680448b5c82
Dmitry Dorogoy <DimonSmart@users.noreply.github.com>
2024-08-07 23:08:49 +0200

    Path finder (#2)
    * Add maze path finder demo and refactor maze components (WIP)
    
    * Path finder prototype added
    
    * Maze path finder algorithm visualization added

--- Demo/Models/MazeCell.cs
+++ Demo/Models/MazeCell.cs
@@ -2,12 +2,37 @@
 
 namespace Demo.Models
 {
-    public class MazeCell : ICell
+    public class MazeCellModel : ICell
     {
+        enum SpecialMarks
+        {
+            None,
+            Start = 1,
+            End = 2
+        }
+
+        private SpecialMarks _specialMark = SpecialMarks.None;
         private bool _wall = false;
+        private int? _waveNumber;
+
+        public bool IsStart() => _specialMark == SpecialMarks.Start;
+
+        public bool IsEnd() => _specialMark == SpecialMarks.End;
 
         public bool IsWall() => _wall;
 
         public void MakeWall() => _wall = true;
+
+        public void SetStart() => _specialMark = SpecialMarks.Start;
+
+        public void SetEnd() => _specialMark = SpecialMarks.End;
+
+        public void ClearSpecialMark() => _specialMark = SpecialMarks.None;
+
+        public void SetWaveNumber(int waveNumber) => _waveNumber = waveNumber;
+
+        public void ResetWaveNumber() => _waveNumber = null;
+
+        public int? WaveNumber => _waveNumber;
     }
 }
