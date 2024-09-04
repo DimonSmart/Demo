50e61caf6211ad2e08818528b1181d1802a5d26d
Dmitrii Dorogoi <dmitrii_dorogoi@epam.com>
2024-09-04 18:00:45 +0200

    WIP

---
+++ Demo/Demos/BJ/BJChromosome.cs
@@ -0,0 +1,12 @@
+﻿using Demo.Demos.TSM.GeneralGenetic;
+
+namespace Demo.Demos.TSM
+{
+    public sealed class BJChromosome : IChromosome<BJChromosome>, IComparable<BJChromosome>
+    {
+        public int CompareTo(BJChromosome? other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
