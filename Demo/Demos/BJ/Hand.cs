6cf7c63559eb8aeb1fbb801c51822cafb6530969
Dmitry Dorogoy <Dmitrii_Dorogoi@epam.com>
2024-09-11 00:22:30 +0200

    BJ PlayerHand class extracted

--- Demo/Demos/BJ/Hand.cs
+++ Demo/Demos/BJ/Hand.cs
@@ -63,27 +63,6 @@
         public bool IsPair => _cards.Count == 2 && _cards[0].Rank == _cards[1].Rank;
     }
 
-    public class PlayerHand(IEnumerable<Card> initialCards, decimal bet, HandOutcome? outcome = null) : Hand(initialCards)
-    {
-        public decimal Bet { get; private set;  } = bet;
-        public HandOutcome? Outcome { get; private set; } = outcome;
-
-        public void DoubleBet() => Bet *= 2;
-
-        public PlayerHand Split()
-        {
-            if (!IsPair) throw new InvalidOperationException("Cannot split a hand that is not a pair.");
-
-            var cardToRemove = _cards[1];
-            _cards.RemoveAt(1);
-            CalculateHandValue();
-
-            return new PlayerHand(new List<Card> { cardToRemove }, Bet);
-        }
-
-        public void SetOutcome(HandOutcome outcome) => Outcome = outcome;
-    }
-
     public class DealerHand(IEnumerable<Card> initialCards) : Hand(initialCards)
     {
         public void FlipSecondCard()
