f16b6cf4f5dff879832743e7aa8d86a057992626
Dmitry Dorogoy <Dmitrii_Dorogoi@epam.com>
2024-10-15 22:06:42 +0200

    Image watermark section added

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Demo;
 using Demo.Services;
+using BlazorImageProcessing;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped<BrowserService>();
+builder.Services.AddScoped<ImageProcessingService>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 await builder.Build().RunAsync();
