a96dec1a88a352a6e718f99f07ff708b6f29dc99
Dmitrii Dorogoi <dmitrii_dorogoi@epam.com>
2024-12-16 13:08:01 +0100

    Markdown buttons added

---
+++ Demo/wwwroot/js/textFunctiones.js
@@ -0,0 +1,10 @@
+﻿window.getSelectionStart = (element) => element.selectionStart;
+window.getSelectionEnd = (element) => element.selectionEnd;
+window.updateTextArea = (element, cursorPosition) => {
+    element.focus();
+    element.setSelectionRange(cursorPosition, cursorPosition);
+};
+window.updateTextAreaContent = (element, value) => {
+    element.value = value;
+    element.dispatchEvent(new Event('input'));
+};
