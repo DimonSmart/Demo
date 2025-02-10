b34dee8304684038cdfbaf080d0a4ec58207aeee
Dmitrii Dorogoi <dmitrii_dorogoi@epam.com>
2025-02-10 17:39:59 +0100

    Feedback template added

---
+++ Demo/wwwroot/js/feedback.js
@@ -0,0 +1,19 @@
+﻿window.captureScreenshot = async function () {
+    var element = document.body;
+    var canvas = await html2canvas(element);
+    return canvas.toDataURL("image/png");
+};
+
+window.openFeedbackDialog = function () {
+    var dlg = document.getElementById("feedbackDialog");
+    if (dlg) {
+        dlg.showModal();
+    }
+};
+
+window.closeFeedbackDialog = function () {
+    var dlg = document.getElementById("feedbackDialog");
+    if (dlg) {
+        dlg.close();
+    }
+};
