8bebe6bca763b1cd151cdb1271ac87203a6b06f6
Dmitrii Dorogoi <Dmitrii_Dorogoi@epam.com>
2025-02-17 22:51:27 +0100

    Maze generation updated to support generic cell parameters

---
+++ Demo/Demos/MazeRunner/MazeRunnerCellModel.cs
@@ -0,0 +1,32 @@
+﻿using DimonSmart.MazeGenerator;
+
+namespace Demo.Demos.MazeRunner
+{
+    public class MazeRunnerCellModel : ICell
+    {
+        enum SpecialMarks
+        {
+            None,
+            Apple = 1,
+            Pear = 2
+        }
+
+        private SpecialMarks _specialMark = SpecialMarks.None;
+
+        private bool _wall = false;
+        private bool _visited = false;
+
+        public bool IsWall() => _wall;
+
+        public void MakeWall() => _wall = true;
+
+        public bool IsApple() => _specialMark == SpecialMarks.Apple;
+        public void SetApple() => _specialMark = SpecialMarks.Apple;
+
+        public bool IsPear() => _specialMark == SpecialMarks.Pear;
+        public void SetPear() => _specialMark = SpecialMarks.Pear;
+
+        public void ClearSpecialMark() => _specialMark = SpecialMarks.None;
+        internal void MarkVisited() => _visited = true;
+    }
+}
