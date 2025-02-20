da535ecfa9a74f14a0ba41483e0a90b7222a25c6
Dmitrii Dorogoi <dorogoj@live.ru>
2025-02-20 16:54:34 +0100

    Kernel and functions template added

---
+++ Demo/Demos/MazeRunner/KernelFactory.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.SemanticKernel;
+
+namespace Demo.Demos.MazeRunner
+{
+    public static class KernelFactory
+    {
+        public static Kernel BuildKernel(ILoggerFactory loggerFactory)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddUserSecrets<Program>()
+                .Build();
+
+            var builder = Kernel.CreateBuilder();
+
+            var httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("http://localhost:11434"),
+                Timeout = TimeSpan.FromMinutes(20)
+            };
+
+#pragma warning disable SKEXP0070
+            builder.AddOllamaChatCompletion(
+                modelId: "llama3.3:70b-instruct-q2_K",
+                httpClient: httpClient,
+                serviceId: "ollama"
+            );
+#pragma warning restore SKEXP0070
+
+            // Register the logger factory and logging
+            builder.Services.AddSingleton(loggerFactory);
+            builder.Services.AddLogging(logging =>
+            {
+                logging.ClearProviders();
+                //  logging.AddProvider(new RegionLoggerProvider());
+                logging.SetMinimumLevel(LogLevel.Trace);
+            });
+
+            var kernel = builder.Build();
+            return kernel;
+        }
+    }
+}
