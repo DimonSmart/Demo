6b98641b2b16475546956a31ef84c40fdff3f075
Dmitrii Dorogoi <Dmitrii_Dorogoi@epam.com>
2025-02-26 22:55:54 +0100

    Ollama model selector added

---
+++ Demo/Demos/MazeRunner/OllamaModelDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Demo.Demos.MazeRunner
+{
+    public class OllamaModelDto
+    {
+        public string name { get; set; }
+        public long size { get; set; }
+        public ModelDetailsDto details { get; set; }
+    }
+
+
+}
