a519d6bf62d5eb9021809a529473b5ddee2d64c4
Dmitrii Dorogoi <Dmitrii_Dorogoi@epam.com>
2025-04-15 20:46:52 +0200

    INTERMEDIATE

--- Demo/Demos/Pdd/QuestionWrapper.cs
+++ Demo/Demos/Pdd/QuestionWrapper.cs
@@ -5,7 +5,8 @@ public class QuestionWrapper
     public QuestionItem Question { get; set; } = null!;
     public QuestionStudyCard StudyCard { get; set; } = null!;
     public bool HasAnswered { get; set; }
-    // public bool IsCorrect { get; set; }
     public AnswerItem? SelectedAnswer { get; set; }
     public bool ShowExplanation { get; set; }
+    public bool ShowSecondaryLanguage1 { get; set; } = false;
+    public bool ShowSecondaryLanguage2 { get; set; } = false;
 }
