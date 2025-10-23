27e3a6d8e4b35f3bf1f7378eb59b41f2e32506f0
Dmitry Dorogoy <DimonSmart@users.noreply.github.com>
2025-10-23 23:43:53 +0200

    Refactor QR sender logic into helpers

---
+++ DemoTests/QrTransfer/QrChunkBuilderTests.cs
@@ -0,0 +1,55 @@
+using QrTransferDemo.Services;
+
+namespace DemoTests.QrTransfer;
+
+public class QrChunkBuilderTests
+{
+    [Fact]
+    public void BuildPackets_SplitsDataIntoExpectedChunks()
+    {
+        var builder = new QrChunkBuilder();
+        var data = Enumerable.Range(0, 200).Select(i => (byte)i).ToArray();
+
+        var packets = builder.BuildPackets("test.bin", data, 64);
+
+        Assert.Equal(4, packets.Count);
+        Assert.All(packets, packet => Assert.Equal(data.Length, packet.FileSize));
+        Assert.Equal(0, packets[0].ChunkIndex);
+        Assert.Equal(3, packets[^1].ChunkIndex);
+        Assert.Equal(4, packets[^1].TotalChunks);
+        Assert.Equal(64, packets[0].Payload.Length);
+        Assert.Equal(64, packets[1].Payload.Length);
+        Assert.Equal(64, packets[2].Payload.Length);
+        Assert.Equal(8, packets[3].Payload.Length);
+    }
+
+    [Fact]
+    public void BuildPackets_ReusesFileIdForSameFile()
+    {
+        var builder = new QrChunkBuilder();
+        var data = new byte[16];
+
+        var first = builder.BuildPackets("document.txt", data, 8);
+        var second = builder.BuildPackets("document.txt", data, 8);
+        var other = builder.BuildPackets("another.txt", data, 8);
+
+        Assert.Equal(first[0].FileId, second[0].FileId);
+        Assert.NotEqual(first[0].FileId, other[0].FileId);
+    }
+
+    [Fact]
+    public void BuildPackets_HandlesEmptyFile()
+    {
+        var builder = new QrChunkBuilder();
+
+        var packets = builder.BuildPackets("empty.bin", Array.Empty<byte>(), 16);
+
+        Assert.Single(packets);
+        var packet = packets[0];
+        Assert.Equal(0, packet.ChunkIndex);
+        Assert.Equal(1, packet.TotalChunks);
+        Assert.Empty(packet.Payload);
+        Assert.Equal(0L, packet.FileSize);
+        Assert.Equal(0u, packet.Crc32);
+    }
+}
